a567814f1c9b613b76b239cc8d8c5ef0725ab34b
Éverton M. Vieira <emuvi@outlook.com.br>
2022-07-27 16:48:34 -0300

    refactors the talker to split the classes in diferent files

---
+++ src/qin-talker-cmd.ts
@@ -0,0 +1,35 @@
+import { QinBody } from "qin_soul";
+import { QinTalker } from "./qin-talker";
+import { IssuedToken } from "./qin-talker-issued";
+
+export class QinTalkerCmd {
+  private readonly _talker: QinTalker;
+
+  public constructor(talker: QinTalker) {
+    this._talker = talker;
+  }
+
+  public list(): Promise<string[]> {
+    return new Promise<string[]>((resolve, reject) => {
+      this._talker
+        .get("/list/cmds")
+        .then((res) => resolve(QinBody.getTextLines(res.data)))
+        .catch((err) => reject(err));
+    });
+  }
+
+  public run(start: CmdRunStart): Promise<IssuedToken> {
+    return new Promise<string>((resolve, reject) => {
+      this._talker
+        .post("/cmd/run", start)
+        .then((res) => resolve(res.data))
+        .catch((err) => reject(err));
+    });
+  }
+}
+
+export type CmdRunStart = {
+  exec: string;
+  args?: string[];
+  input?: string[];
+};
---
+++ src/qin-talker-issued.ts
@@ -0,0 +1,99 @@
+import { QinTalker } from "./qin-talker";
+import { QinBody } from "qin_soul"
+
+export class QinTalkerIssued {
+  private readonly _talker: QinTalker;
+
+  public constructor(talker: QinTalker) {
+    this._talker = talker;
+  }
+
+  public ask(question: IssuedQuestion): Promise<IssuedAnswer> {
+    return new Promise<IssuedAnswer>((resolve, reject) => {
+      this._talker
+        .post("/issued", question)
+        .then((res) => {
+          const lines = QinBody.getTextLines(res.data);
+          let answer: IssuedAnswer = {};
+          let i = 0;
+          let gettingResultLines = false;
+          while (i < lines.length) {
+            if (!gettingResultLines) {
+              switch (lines[i].trim()) {
+                case "Created At:":
+                  i++;
+                  answer.createdAt = +lines[i].trim();
+                  break;
+                case "Result Coded:":
+                  i++;
+                  answer.resultCoded = +lines[i].trim();
+                  break;
+                case "Is Done:":
+                  i++;
+                  answer.isDone = lines[i].trim() === "true";
+                  break;
+                case "Finished At:":
+                  i++;
+                  answer.finishedAt = +lines[i].trim();
+                  break;
+                case "Result Lines:":
+                  answer.resultLines = [];
+                  gettingResultLines = true;
+                  break;
+                default:
+                  break;
+              }
+            } else {
+              answer.resultLines.push(lines[i]);
+            }
+            i++;
+          }
+          resolve(answer);
+        })
+        .catch((err) => reject(err));
+    });
+  }
+
+  public askWhenDone(question: IssuedQuestion): Promise<IssuedAnswer> {
+    return new Promise<IssuedAnswer>((resolve, reject) => {
+      const questionIsDone = {
+        token: question.token,
+        askIsDone: true,
+      } as IssuedQuestion;
+      const askIsDone = () => {
+        this.ask(questionIsDone)
+          .then((res) => {
+            if (res.isDone) {
+              this.ask(question)
+                .then((res) => resolve(res))
+                .catch((err) => reject(err));
+            } else {
+              setTimeout((_) => askIsDone(), 700);
+            }
+          })
+          .catch((err) => reject(err));
+      };
+      askIsDone();
+    });
+  }
+}
+
+export type IssuedToken = string;
+
+export type IssuedQuestion = {
+  token: string;
+  askCreatedAt?: boolean;
+  askResultLines?: boolean;
+  askResultLinesFrom?: number;
+  askResultCoded?: boolean;
+  askIsDone?: boolean;
+  askFinishedAt?: boolean;
+};
+
+export type IssuedAnswer = {
+  createdAt?: number;
+  resultLines?: string[];
+  resultCoded?: number;
+  isDone?: boolean;
+  finishedAt?: number;
+};
