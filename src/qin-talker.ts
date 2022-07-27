83fc9dc54c15bde633113ee4f2096f2587efbeb4
Éverton M. Vieira <emuvi@outlook.com.br>
2022-07-27 18:08:13 -0300

    adds the parameters calls

--- src/qin-talker.ts
+++ src/qin-talker.ts
@@ -2,16 +2,19 @@ import axios, { AxiosResponse } from "axios";
 import { QinChief } from "./qin-chief";
 import { QinTalkerCmd } from "./qin-talker-cmd";
 import { QinTalkerIssued } from "./qin-talker-issued";
+import { QinTalkerParam } from "./qin-talker-param";
 
 export class QinTalker {
   private readonly _chief: QinChief;
   private readonly _cmd: QinTalkerCmd;
   private readonly _issued: QinTalkerIssued;
+  private readonly _param: QinTalkerParam;
 
   public constructor(chief: QinChief) {
     this._chief = chief;
     this._cmd = new QinTalkerCmd(this);
     this._issued = new QinTalkerIssued(this);
+    this._param = new QinTalkerParam(this);
   }
 
   public get(address: string, headers?: any): Promise<AxiosResponse<never>> {
@@ -39,5 +42,9 @@ export class QinTalker {
 
   public get issued() {
     return this._issued;
+  }
+
+  public get param() {
+    return this._param;
   }
 }
