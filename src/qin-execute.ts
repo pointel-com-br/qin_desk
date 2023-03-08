8c0405e5254ce2df6d2d9227344f29d1fbe807c6
Éverton M. Vieira <emuvi@outlook.com.br>
2023-03-08 18:41:19 -0300

    adds the log level

--- src/qin-execute.ts
+++ src/qin-execute.ts
@@ -3,4 +3,13 @@ export type QinExecute = {
   args?: string[];
   input?: string[];
   joinErrs?: boolean;
+  logLevel?: QinLogLevel;
 };
+
+export enum QinLogLevel {
+  LEVEL_ERROR = 0,
+  LEVEL_WARN = 1,
+  LEVEL_INFO = 2,
+  LEVEL_DEBUG = 3,
+  LEVEL_TRACE = 4,
+}
